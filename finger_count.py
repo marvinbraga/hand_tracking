03a26e1bc70fa220c04492714b6ffc7dffb80448
Marcus Vinicius Braga <marvinbraga@users.noreply.github.com>
2021-04-04 17:45:52 -0300

    Fingers count

---
+++ finger_count.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+"""
+Project Finger Count
+"""
+import cv2
+from hand_tracking import FpsShowInfo, HandPointsBold, HandDetector, HandLandMarks
+
+
+# Cria o HandDetector.
+detector = HandDetector(
+    fps=FpsShowInfo(color=(90, 90, 90)),
+    bold_points=HandPointsBold(bold_points=[
+        HandLandMarks.INDEX_FINGER_MPC,
+        HandLandMarks.THUMB_TIP,
+        HandLandMarks.INDEX_FINGER_PIP,
+        HandLandMarks.INDEX_FINGER_TIP,
+        HandLandMarks.MIDDLE_FINGER_PIP,
+        HandLandMarks.MIDDLE_FINGER_TIP,
+        HandLandMarks.RING_FINGER_PIP,
+        HandLandMarks.RING_FINGER_TIP,
+        HandLandMarks.PINK_PIP,
+        HandLandMarks.PINK_TIP
+    ])
+)
+
+# Inicializa o OpenCV.
+cap = cv2.VideoCapture(0)
+cam_height, cam_width = 640, 480
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+
+while True:
+    success, img = cap.read()
+    img = detector.find_hands(img)
+    # Se recuperou a posição dos dedos.
+    landmarks = detector.points
+    if landmarks:
+        # Thumb
+        fingers = []
+        if landmarks[0][2] < landmarks[1][2]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # Outros dedos
+        for index in range(2, 10, 2):
+            if landmarks[index + 1][2] < landmarks[index][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        total_fingers = fingers.count(1)
+        print(total_fingers, fingers)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
