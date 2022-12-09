c5d012f35120371e65098fc7440f5b42ec069732
Marcus Braga <mvbraga@gmail.com>
2022-12-09 01:24:53 -0300

    LinkedIn course opencv cap 3

--- apps/linked_in/opencv/03_04.py
+++ apps/linked_in/opencv/03_04.py
@@ -8,10 +8,10 @@ h, s, v = hsv[:, :, 0], hsv[:, :, 1], hsv[:, :, 2]
 hsv_split = np.concatenate((h, s, v), axis=1)
 cv2.imshow("Split HSV", hsv_split)
 
-ret, min_sat = cv2.threshold(s, 40, 255, cv2.THRESH_BINARY)
+_, min_sat = cv2.threshold(s, 40, 255, cv2.THRESH_BINARY)
 cv2.imshow("Sat Filter", min_sat)
 
-ret, max_hue = cv2.threshold(h, 15, 255, cv2.THRESH_BINARY_INV)
+_, max_hue = cv2.threshold(h, 15, 255, cv2.THRESH_BINARY_INV)
 cv2.imshow("Hue Filter", max_hue)
 
 final = cv2.bitwise_and(min_sat, max_hue)
